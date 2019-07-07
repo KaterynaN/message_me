f750713d7758a96d9bdf9235cafe94893a67ead1
KaterynaN <kateryna.od.ua@gmail.com>
2019-07-07 22:46:04 +0300

    Add User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"Jack", password:"123456")
+User.create(username:"Mark", password:"123456")
+User.create(username:"Ann", password:"123456")
+User.create(username:"Kate", password:"123456")
+User.create(username:"Sarah", password:"123456")
